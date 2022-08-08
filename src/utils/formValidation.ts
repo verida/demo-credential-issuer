9c17b919defab352d8a2f76f063e7f95e3bbfe47
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-08-09 00:09:21 +0100

    fix: modified error message for validation schema #98

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -12,12 +12,7 @@ const today = new Date();
 
 export const formSchema = yup.object({
   firstName: yup.string().required().label("First name"),
-  did: yup
-    .string()
-    .required()
-    .min(50)
-    .max(50)
-    .label("Valid did must contain 50 characters"),
+  did: yup.string().required().min(50).max(50).label("Valid DID"),
   lastName: yup.string().required().min(2).label("Last name"),
   regNumber: yup
     .number()
@@ -25,5 +20,5 @@ export const formSchema = yup.object({
     .min(5)
     .label("Registration Number must be greater than or equal to 5"),
   regExpDate: yup.date().transform(parseDateString).min(today).required(),
-  healthType: yup.string().required(),
+  healthType: yup.string().required("Health Type"),
 });
