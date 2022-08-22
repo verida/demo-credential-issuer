b4a9d93f3a528a127e318865ac1aa1114ac65186
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-08-22 09:55:42 +0100

    fix:updated the yup schema validation #98

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -11,14 +11,10 @@ const parseDateString = (value: string, originalValue: string) => {
 const today = new Date();
 
 export const formSchema = yup.object({
-  firstName: yup.string().trim().required().label("First name"),
-  did: yup.string().required().min(50).max(50).label("Valid DID"),
-  lastName: yup.string().required().min(2).label("Last name"),
-  regNumber: yup
-    .number()
-    .required()
-    .min(5, "Registration Number must be greater than or equal to 5")
-    .label("Registration Number"),
+  firstName: yup.string().trim().required().label("First Name"),
+  did: yup.string().required().length(50).label("DID"),
+  lastName: yup.string().trim().required().min(2).label("Last Name"),
+  regNumber: yup.number().required().min(5).label("Registration Number"),
   regExpDate: yup
     .date()
     .transform(parseDateString)
